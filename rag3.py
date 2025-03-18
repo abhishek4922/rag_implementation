4f10feea04dd7184f41743a927c6b70a3b138ad4
abhishek4922 <abhishekjain18138@gmail.com>
2025-03-18 19:31:49 +0530

    changed

--- rag3.py
+++ rag3.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
-# GOOGLE_API_KEY = "AIzaSyC99O1JZAAd3KEI-u0o_jsGwII2PFflvlE"  
+
 genai.configure(api_key=GOOGLE_API_KEY)
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=GOOGLE_API_KEY)
 
